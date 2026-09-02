1aab6331bdb24fbef22700b204b48fd468c7b239
mvetaires <mvetairesbas2@gmail.com>
2026-09-01 23:38:45 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBex8pwooVrHax80MuEB2ELacA_9xfRFQs",
   authDomain: "levantamento-cvu.firebaseapp.com",
@@ -12,6 +6,3 @@ const firebaseConfig = {
   messagingSenderId: "609291484175",
   appId: "1:609291484175:web:d1300f9c37fa6d41f9777a"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
